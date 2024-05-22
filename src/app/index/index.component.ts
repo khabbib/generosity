780e0b79b8d1f3b121919fbd0adb6ff8ff12ff95
SossioG <sosiosos2002@gmail.com>
2024-05-22 09:23:54 +0200

    modified index page (the donation page), added thank you page and function submitForm in index.components.ts

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-index',
@@ -7,7 +8,35 @@ import {Component} from '@angular/core';
 })
 export class IndexComponent {
 
-  constructor() {
+  projectName: string = '';
+  donationAmount: string = '';
+
+  constructor(private router: Router) {
+    //alert("Note! Index is Donation page.");
+  }
+
+  // handle form submission logic when donation button is clicked
+  submitForm() {
+    // set items to local storage
+    localStorage.setItem("project-name", this.projectName);
+    localStorage.setItem("donation-amount", this.donationAmount);
+
+    // console log to check the values
+    console.log('Form submitted!');
+    console.log('Project Name:', this.projectName);
+    console.log('Donation Amount:', this.donationAmount);
+    alert("DONE");
+
+    // CALL API HERE
+    // ...
+
+    // success or failed
+    if(true) { // if payment went well
+      this.router.navigateByUrl('/thankyou');
+    } else { // payment failed
+      alert("Failed to donate. Please try again");
+    }
+
   }
 
 }
